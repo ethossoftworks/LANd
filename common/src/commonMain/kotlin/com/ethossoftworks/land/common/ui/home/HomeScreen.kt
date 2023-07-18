4dc522c1e65b8c9569331e83769c3780d77da1d1
Ryan Mitchener <ryan@outsidesource.com>
2023-07-18 00:08:07 -0400

    Animated InfoMessage

--- common/src/commonMain/kotlin/com/ethossoftworks/land/common/ui/home/HomeScreen.kt
+++ common/src/commonMain/kotlin/com/ethossoftworks/land/common/ui/home/HomeScreen.kt
@@ -11,12 +11,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -26,6 +24,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -161,17 +160,25 @@ fun HomeScreen(
 
 @Composable
 private fun BoxScope.TransferMessage(
-    fileTransfer: FileTransfer?,
+    transfer: FileTransfer?,
     interactorRef: ValRef<HomeScreenViewInteractor>,
 ) {
     val interactor = interactorRef.value
 
-    Box(
+    KeyedAnimatedContent(
+        targetState = transfer,
         modifier = Modifier.align(if (Platform.current.isDesktop) Alignment.BottomEnd else Alignment.BottomCenter),
-    ) {
-        if (fileTransfer == null) return@Box
+    ) { fileTransfer, transition ->
+        if (fileTransfer == null) return@KeyedAnimatedContent
+        val alphaAnim by transition.animateFloat(transitionSpec = { tween(250) }) { if (it == fileTransfer) 1f else 0f }
+        val positionAnim by transition.animateFloat(transitionSpec = { tween(250) }) { if (it == fileTransfer) 0f else 10f }
+
         Column(
             modifier = Modifier
+                .graphicsLayer {
+                    alpha = alphaAnim
+                    translationY = positionAnim
+                }
                 .width(350.dp)
                 .padding(AppTheme.dimensions.screenHPadding, AppTheme.dimensions.screenVPadding)
                 .windowInsetsPadding(KMPWindowInsets.bottomInsets)
@@ -441,5 +448,54 @@ private fun DiscoveredDevice(
             maxLines = 2,
             lineHeight = 1.2.em,
         )
+    }
+}
+
+@Composable
+private fun <T> KeyedAnimatedContent(
+    targetState: T,
+    modifier: Modifier = Modifier,
+    contentKey: (targetState: T) -> Any? = { it },
+    content: @Composable (targetState: T, transition: Transition<T>) -> Unit,
+) {
+    val transition = updateTransition(targetState)
+    val currentlyVisible = remember { mutableStateListOf<T>().apply { add(transition.currentState) } }
+    val contentMap = remember { mutableMapOf<T, @Composable () -> Unit>() }
+
+    if (transition.currentState == transition.targetState) {
+        // If not animating, just display the current state
+        if (currentlyVisible.size != 1 || currentlyVisible[0] != transition.targetState) {
+            // Remove all the intermediate items from the list once the animation is finished.
+            currentlyVisible.removeAll { it != transition.targetState }
+            contentMap.clear()
+        }
+    }
+
+    if (!contentMap.contains(transition.targetState)) {
+        // Replace target with the same key if any
+        val replacementId = currentlyVisible.indexOfFirst {
+            contentKey(it) == contentKey(transition.targetState)
+        }
+
+        if (replacementId == -1) {
+            currentlyVisible.add(transition.targetState)
+        } else {
+            currentlyVisible[replacementId] = transition.targetState
+        }
+
+        contentMap.clear()
+        currentlyVisible.forEach { stateForContent ->
+            contentMap[stateForContent] = {
+                content(stateForContent, transition)
+            }
+        }
+    }
+
+    Box(modifier) {
+        currentlyVisible.forEach {
+            key(contentKey(it)) {
+                contentMap[it]?.invoke()
+            }
+        }
     }
 }
