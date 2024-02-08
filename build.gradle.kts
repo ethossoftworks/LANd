b7f8972f238afff7b2b4db0c76a73d5a9c1e38cd
Ryan Mitchener <ryan@outsidesource.com>
2024-02-07 21:51:06 -0500

    Removed old maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,6 @@ allprojects {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven {
-            url = uri("https://maven.pkg.github.com/outsidesource/OSKit-KMP")
-            credentials {
-                val credentialProperties = java.util.Properties()
-                File(rootDir, "credential.properties").reader().use { stream -> credentialProperties.load(stream) }
-                username = credentialProperties["username"].toString()
-                password = credentialProperties["password"].toString()
-            }
-        }
     }
 }
 
