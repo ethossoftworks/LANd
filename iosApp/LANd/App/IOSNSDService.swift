80acb63abcddcb31440de8c2ca98ff0fca3ce446
Ryan Mitchener <ryan@outsidesource.com>
2024-04-07 00:03:32 -0400

    Added getting local IP address

--- iosApp/LANd/App/IOSNSDService.swift
+++ iosApp/LANd/App/IOSNSDService.swift
@@ -8,7 +8,43 @@ class IOSNSDService : INSDService {
     func __doInit() async throws {}
            
     func __getLocalIpAddress() async throws -> String {
-        return ""
+        // Adapted from: https://gist.github.com/camyoh/aa341e4e40afaa40c84813d899369566
+        var address : String?
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return "0.0.0.0" }
+        guard let firstAddr = ifaddr else { return "0.0.0.0" }
+        
+        // For each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            
+            // Check for IPv4 or IPv6 interface:
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                // Check interface name:
+                let name = String(cString: interface.ifa_name)
+                if  name == "en0" {
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                } else if (name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3") {
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(1), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        
+        return address ?? "0.0.0.0"
     }
  
     func __observeServiceTypes() async throws -> SkieSwiftFlow<NSDServiceType> {
@@ -65,7 +101,7 @@ private class ServiceFlow : SwiftFlow<NSDServiceEvent> {
                 case .removed(let result):
                     let serviceName = serviceNameFromResult(result: result)
                     self.tryEmit(value: NSDServiceEvent.ServiceRemoved(service: NSDServicePartial(type: self.type, name: serviceName)))
-                case .changed(let oldResult, let newResult, let flags):
+                case .changed(let oldResult, let newResult, _):
                     let serviceName = serviceNameFromResult(result: newResult)
                     self.tryEmit(value: NSDServiceEvent.ServiceAdded(service: NSDServicePartial(type: self.type, name: serviceName)))
                 default:
