507d1d46a7a4557e4ecd21ee78a03745f642de82
Ryan Mitchener <ryan@outsidesource.com>
2024-04-27 00:25:39 -0400

    Updated test

--- composeApp/src/commonTest/kotlin/com/ethossoftworks/land/lib/crypto/Encryption.kt
+++ composeApp/src/commonTest/kotlin/com/ethossoftworks/land/lib/crypto/Encryption.kt
@@ -7,6 +7,7 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class Encryption {
+    @OptIn(ExperimentalStdlibApi::class)
     @Test
     fun encryption() {
         val alicePrivateKey = DHKey.generatePrivateKey(2_048)
@@ -16,7 +17,7 @@ class Encryption {
 
         val iv = SecureRandom.nextBytes(16)
 
-        val original = byteArrayOf(0x00.toByte(), 0x01.toByte(), 0x02.toByte())
+        val original = "000102030405060708090A0B0C0D0E0F".hexToByteArray()
         val encrypted = AES.encryptAesCtr(original, sharedKey, iv, CipherPadding.PKCS7Padding)
         val decrypted = AES.decryptAesCtr(encrypted, sharedKey, iv, CipherPadding.PKCS7Padding)
 
