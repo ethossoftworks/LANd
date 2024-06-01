15828dedbc94e54f2c43a317bd5479a4725b8104
Ryan Mitchener <ryan@outsidesource.com>
2024-05-31 23:45:08 -0400

    Fixed issues with oskit-kmp file handler

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -17,6 +17,34 @@ val buildInfoProps = Properties().apply {
 version = buildInfoProps["version"] as? String ?: "0.0.0"
 val buildNumber = buildInfoProps["build"]?.toString()?.toInt() ?: 1
 
+val lwjglVersion = "3.3.3"
+
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } -> {
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) }) {
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            } else {
+                "natives-linux"
+            }
+        }
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } -> {
+            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+        }
+        arrayOf("Windows").any { name.startsWith(it) } -> {
+            if (arch.contains("64")) {
+                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+            } else {
+                "natives-windows-x86"
+            }
+        }
+        else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
+
 kotlin {
     listOf(
         iosX64(),
@@ -90,6 +118,8 @@ kotlin {
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
+            runtimeOnly("org.lwjgl:lwjgl:$lwjglVersion:$lwjglNatives")
+            runtimeOnly("org.lwjgl:lwjgl-tinyfd:$lwjglVersion:$lwjglNatives")
         }
     }
 }
